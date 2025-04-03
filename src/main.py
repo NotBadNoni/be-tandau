265394fd36c40b273d7734aa4b8fbbfbbe3726a5
NotBadNoni <noni8609@gmail.com>
2025-04-04 01:54:32 +0500

    feat: created university router and included to the main router

--- src/main.py
+++ src/main.py
@@ -8,16 +8,18 @@ from src.core.store import init_db
 from src.di.app_provider import create_container
 from src.routers import auth
 from src.routers import user
+from src.routers import university
 
 container = create_container()
 
 
 def create_app():
     app = FastAPI()
+    app.mount('/media', StaticFiles(directory=MEDIA_DIR), name='media')
     setup_dishka(container=container, app=app)
     app.include_router(router=auth.router, tags=["auth"], prefix="/auth")
     app.include_router(router=user.router, tags=["user"], prefix="/users")
-    app.mount('/media', StaticFiles(directory=MEDIA_DIR), name='media')
+    app.include_router(router=university.router, tags=["university"], prefix="/universities")
     return app
 
 
---
+++ src/routers/university.py
@@ -0,0 +1,53 @@
+from dishka import FromDishka
+from dishka.integrations.fastapi import inject
+from fastapi import APIRouter
+
+from src.controllers.university import UniversityController
+from src.schemas.university import CreateUniversity, UpdateUniversity, UniversityResponse
+
+router = APIRouter(prefix="/universities", tags=["Universities"])
+
+
+@router.get("/", response_model=list[UniversityResponse])
+@inject
+async def get_universities(
+        controller: FromDishka[UniversityController],
+):
+    return await controller.get_universities()
+
+
+@router.get("/{university_id}", response_model=UniversityResponse)
+@inject
+async def get_university(
+        university_id: int,
+        controller: FromDishka[UniversityController],
+):
+    return await controller.get_university(university_id)
+
+
+@router.post("/", response_model=UniversityResponse)
+@inject
+async def create_university(
+        form: CreateUniversity,
+        controller: FromDishka[UniversityController],
+):
+    return await controller.create_university(form.dict())
+
+
+@router.put("/{university_id}")
+@inject
+async def update_university(
+        university_id: int,
+        form: UpdateUniversity,
+        controller: FromDishka[UniversityController],
+):
+    return await controller.update_university(university_id, form.dict(exclude_unset=True))
+
+
+@router.delete("/{university_id}")
+@inject
+async def delete_university(
+        university_id: int,
+        controller: FromDishka[UniversityController],
+):
+    return await controller.delete_university(university_id)
