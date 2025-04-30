43f4c25ede01cbfefde1113eee1452ffb1f85920
NotBadNoni <noni8609@gmail.com>
2025-04-30 18:21:56 +0500

    feat(config): fixed oauth google register config

--- src/core/config.py
+++ src/core/config.py
@@ -65,9 +65,6 @@ oauth.register(
     name='google',
     client_id=settings.GOOGLE_CLIENT_ID,
     client_secret=settings.GOOGLE_CLIENT_SECRET,
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'},
 )
