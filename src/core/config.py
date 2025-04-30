2b829bfa434aff3299ec2c671299194dd00b421f
NotBadNoni <noni8609@gmail.com>
2025-04-30 18:32:24 +0500

    feat(config): added api base url in oauth scope

--- src/core/config.py
+++ src/core/config.py
@@ -66,5 +66,6 @@ oauth.register(
     client_id=settings.GOOGLE_CLIENT_ID,
     client_secret=settings.GOOGLE_CLIENT_SECRET,
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    api_base_url="https://openidconnect.googleapis.com/v1/",
     client_kwargs={'scope': 'openid email profile'},
 )
